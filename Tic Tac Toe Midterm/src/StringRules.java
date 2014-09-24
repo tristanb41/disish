21d98dbbce531bdd369d1a0b90eaad2f90f0555f
Tristan.Beaudoin <Tristan.Beaudoin@MU-C3-04.mullen.pvt.k12.co.us>
2014-09-24 15:30:32 -0600

    disish

---
+++ Tic Tac Toe Midterm/src/StringRules.java
@@ -0,0 +1,17 @@
+
+public class StringRules
+	{
+
+	public static void main(String[] args)
+		{
+		// TODO Auto-generated method stub
+
+		}
+
+	public void inventory(String string)
+		{
+		// TODO Auto-generated method stub
+		
+		}
+
+	}
---
+++ Tic Tac Toe Midterm/src/game.java
@@ -0,0 +1,19 @@
+
+public class game
+	{
+
+	public static void main(String[] args) throws InterruptedException
+	
+		{
+		XandO();
+		}
+
+	private static void XandO() throws InterruptedException
+		{
+		Main.play();
+
+		}
+	}
+
+	
+		
